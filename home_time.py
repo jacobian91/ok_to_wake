cc556feb2e578dfdd0b8ba94361ab67fde80fe33
Jacob Erickson <jacob.erickson@wibotic.com>
2022-11-15 23:37:16 -0800

    Limit time syncing

--- home_time.py
+++ home_time.py
@@ -1,26 +1,36 @@
 import time
 
+import ntptime
 import urequests as requests
-
-# import ntptime
 
 try:  # Type checking
     import typing
 
     if typing.TYPE_CHECKING:
         from typing import Dict
+
         import requests
 except:
     pass
 
-# ntptime.settime()
 TIME_NAMES = ["year", "day", "month", "hour", "min", "sec", "ms"]
+
+local_details = {"last_sync": 0, "offset": 0}
+TWLEVE_HOURS = 12 * 60 * 60
 
 
 def get_home_time() -> Dict[str, int]:
-    response = requests.get(
-        "http://worldtimeapi.org/api/timezone/America/Los_Angeles"
-    ).json()
+    if time.time() - TWLEVE_HOURS > local_details["last_sync"]:
+        print("New Time Sync")
+        ntptime.settime()
+        local_details["last_sync"] = time.time()
+        response = requests.get(
+            "http://worldtimeapi.org/api/timezone/America/Los_Angeles"
+        ).json()
+        local_details["offset"] = response["raw_offset"]
+        unixtime = response["unixtime"]
+    else:
+        unixtime = time.time()
 
-    home_time: tuple = time.localtime(response["unixtime"] + response["raw_offset"])
+    home_time: tuple = time.localtime(unixtime + local_details["offset"])
     return dict(zip(TIME_NAMES, home_time))
