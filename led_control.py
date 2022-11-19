8493e47e749601d1cde78527d2184c99c89dbe93
Jacob Erickson <jacob.erickson@wibotic.com>
2022-11-18 20:05:14 -0800

    Add named colors and make led control sequence

--- led_control.py
+++ led_control.py
@@ -15,6 +15,16 @@ pwm_r.freq(PWM_FREQ)
 pwm_g.freq(PWM_FREQ)
 pwm_b.freq(PWM_FREQ)
 
+RED = (100, 0, 0)
+GREEN = (0, 100, 0)
+DIMGREEN = (0, 60, 0)
+BLUE = (0, 0, 100)
+WHITE = (100, 100, 100)
+BLUEGREEN = (0, 100, 100)
+PURPLE = (100, 0, 100)
+YELLOW = (100, 20, 0)
+DIMWHITE = (10, 10, 10)
+
 
 def color_pwm_set(r, g, b):
     pwm_r.duty_u16(int(r * PWM_100_U16 / 100))
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from machine import Timer
 
 import led_control
 
-led_control.color_pwm_set(0, 0, 100)  # Blue
+led_control.color_pwm_set(*led_control.BLUE)
 led_control.run_heartbeat(period=200)
 import connect
 
@@ -13,24 +13,32 @@ from webrepl import webrepl
 webrepl.start()
 
 led_control.run_heartbeat(period=4000)
-led_control.color_pwm_set(0, 100, 0)  # Green
+led_control.color_pwm_set(*led_control.WHITE)
+
+LIGHT_TIMES = [
+    (1700, led_control.RED),
+    (900, led_control.DIMWHITE),
+    (630, led_control.GREEN),
+    (500, led_control.DIMGREEN),
+    (430, led_control.YELLOW),
+    (0, led_control.RED),
+]
 
 
 def led_check(_):
     current_time = home_time.get_home_time()
-    print(current_time["sec"])
-    if current_time["sec"] > 45:
-        led_control.color_pwm_set(100, 0, 0)
-    elif current_time["sec"] > 30:
-        led_control.color_pwm_set(0, 100, 0)
-    elif current_time["sec"] > 15:
-        led_control.color_pwm_set(0, 0, 100)
-    else:
-        led_control.color_pwm_set(100, 100, 100)
+    time_s = [f"{name}={current_time[name]}" for name in home_time.TIME_NAMES]
+    time_s = " ".join(time_s)
+    print(time_s)
+    current_hour_min = current_time["hour"] * 100 + current_time["sec"]
+    for light_time, light_color in LIGHT_TIMES:
+        if current_hour_min >= light_time:
+            led_control.color_pwm_set(*light_color)
+            break
 
 
 led_check_timer = Timer()
-led_check_timer.init(period=3000, mode=Timer.PERIODIC, callback=led_check)
+led_check_timer.init(period=20000, mode=Timer.PERIODIC, callback=led_check)
 
 
 def stop():
