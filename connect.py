282e0256d9a41f2ce5ee41135854bf14cff14fb7
Jacob Erickson <jacob.erickson@wibotic.com>
2022-11-18 16:58:55 -0800

    Refactor connection function to be cleaner

--- connect.py
+++ connect.py
@@ -5,23 +5,34 @@ import credentials
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 
+
+class WLAN_STATUSES:
+    STAT_WRONG_PASSWORD = -3
+    STAT_NO_AP_FOUND = -2
+    STAT_CONNECT_FAIL = -1
+    STAT_IDLE = 0
+    STA_IF = 0
+    AP_IF = 1
+    UNKNOWN = 2
+    STAT_CONNECTING = 1
+    STAT_GOT_IP = 3
+
+
 # Wait for connect or fail
 def reconnect():
     while True:
         wlan.connect(credentials.wifi_ssid, credentials.wifi_pass)
         while True:
-            if wlan.status() < 0 or wlan.status() >= 3:
-                break
-            print("waiting for connection...")
             time.sleep(1)
-
-        # Handle connection error
-        if wlan.status() != 3:
-            print("connection failed")
-            continue
-            raise RuntimeError("network connection failed")
-        else:
-            print("connected")
-            status = wlan.ifconfig()
-            print("ip = " + status[0])
-            break
+            status = wlan.status()
+            print(f"Network Status={status}")
+            if status == WLAN_STATUSES.STAT_GOT_IP:
+                config = wlan.ifconfig()
+                print("Connected IP = " + config[0])
+                return
+            elif status == WLAN_STATUSES.STAT_CONNECTING:
+                print("Waiting for Connection...")
+                continue
+            else:
+                print("Connection Failed")
+                break
